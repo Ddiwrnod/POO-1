15a77e84ad70daa1e770696176b36d798ad9284a
Nelson <Nelson@Acer_Nelson>
2016-08-28 12:56:04 -0500

    Final Loteria

--- src/NewClass.java
+++ src/NewClass.java
@@ -11,6 +11,13 @@
 public class NewClass {
 
 public static void main(String[] args) {
+    
+    System.out.println("Bienvenido a UN Lotería \n"
+            + "Ingrese 3 cifras y participe por los siguientes premios: \n"
+            + "Acierte 1 cifra en cualquier orden: $10 \n"
+            + "Acierte 2 cifras en cualquier orden: $100 \n"
+            + "Acierte 3 cifras en cualquier orden: $1.000 \n"
+            + "Acierte 3 cifras en orden: $1.000.000 \n");
     int a,b,c,d,e,f,g,h=0,i=0;
     d=(int)(Math.random()*10);
     e=(int)(Math.random()*10);
@@ -57,15 +64,37 @@ public static void main(String[] args) {
     if (d==e||d==f||e==f){
         h=h-1;
     }
+    if ((d==e||d==f||e==f)&&(a==b||b==c||a==c)){
+        h=h+1;
+        i=i+1;
+    }
     System.out.println("Y usted acertó en: "+h+" cifras con "+i+" cifras en orden");
+    
+    PremioGanado(h,i);
     }
 
-public static void Premios (int premio){
+public static void PremioGanado(int Cifras, int Orden){
     
-    int resultado = premio;
-    
-    System.out.println("El resultado de la suma fue: " + resultado);
-    
+    if (Orden==3){
+        System.out.println("Su premio es: $1.000.000");
+    }
+    else {
+        switch (Cifras) {
+            case 1:
+                System.out.println("Su premio es: $10");
+                break;
+            case 2:
+                System.out.println("Su premio es: $100");
+                break;
+            case 3:
+                System.out.println("Su premio es: $1000");
+                break;
+            default:
+                System.out.println("Usted no ha gannado en esta oportunidad, intentelo de nuevo");
+                break;
+        }
+        
+    }
   }
 
 }
